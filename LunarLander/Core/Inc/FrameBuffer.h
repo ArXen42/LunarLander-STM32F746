8026da883d23c48c9d4f1cb2b844e8357ff39747
Georgii Pelageikin <arxen42@tutanota.com>
2020-01-12 03:02:25 +0300

    Minimal CubeMX configuration

---
+++ LunarLander/Core/Inc/FrameBuffer.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdint.h>
+#include <string.h>
+
+extern uint16_t  FrameBuffer[480 * 272 * (5 + 6 + 5) / 16];
---
+++ LunarLander/Core/Src/FrameBuffer.cpp
@@ -0,0 +1,3 @@
+#include "FrameBuffer.h"
+
+uint16_t FrameBuffer[480 * 272 * (5 + 6 + 5) / 16];
